5748dbc1c7f631e9bba13314c6171320cf7b2d93
Igor Kazakov <igor.kazakov157@gmail.com>
2018-02-27 20:11:42 +0300

    css and image assets added

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,39 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                          mozjpeg: {
+                            progressive: true,
+                            quality: 65
+                          },
+                          pngquant: {
+                            quality: '65-90',
+                            speed: 4
+                          }
+                        }
+                      }
+                ]
+            }
+        ]
+    }
 };
