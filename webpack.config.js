109314b6ff1b139461c1e7fe1208f23eb858e811
Igor Kazakov <igor.kazakov157@gmail.com>
2018-02-28 15:32:39 +0300

    Prevent Duplication

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,22 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
-        app: './src/index.js',
-        print: './src/print.js'
+        index: './src/index.js',
+        another: './src/another-module.js'
     },
     plugins: [
-        new CleanWebpackPlugin(['dist']),
-        new HtmlWebpackPlugin({
-            title: 'Output Management'
+        new HTMLWebpackPlugin({
+            title: 'Code Splitting'
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'common' // Specify the common bundle's name.
         })
     ],
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
-    },
-
+    }
 };
